f264631ee6b8de0c62cf0b2ef832b59f05c96ad6
andrewpbray <andrew.bray@gmail.com>
2016-04-08 11:34:32 -0700

    initial commit.

---
+++ assets/week-10/pca-code.R
@@ -0,0 +1,11 @@
+# Slides from last time: http://andrewpbray.github.io/math-243/assets/week-10/PCA.Rmd
+
+d <- read.csv("http://andrewpbray.github.io/math-243/assets/data/handwritten.csv")
+
+plot_letter <- function(x) {
+  a <- as.numeric(x[, -1])
+  m <- matrix(a, nrow = 8, byrow = TRUE)
+  m <- t(apply(m, 2, rev)) # rotate matrix
+  image(m, axes = FALSE, col = rev(grey(seq(0, 1, length = 256))))
+  box()
+}
