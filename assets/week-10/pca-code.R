33c7a3fd110ab00e2484a32dc83c424416d7743b
andrewpbray <andrew.bray@gmail.com>
2016-04-08 11:36:14 -0700

    add comment.

--- assets/week-10/pca-code.R
+++ assets/week-10/pca-code.R
@@ -2,7 +2,7 @@
 
 d <- read.csv("http://andrewpbray.github.io/math-243/assets/data/handwritten.csv")
 
-plot_letter <- function(x) {
+plot_letter <- function(x) { # x should be a row from the handwritten data set
   a <- as.numeric(x[, -1])
   m <- matrix(a, nrow = 8, byrow = TRUE)
   m <- t(apply(m, 2, rev)) # rotate matrix
