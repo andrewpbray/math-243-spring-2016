4a074ddc95a6f23b2a36bec7ae17fb416f78cc30
andrewpbray <andrew.bray@gmail.com>
2016-04-08 11:43:57 -0700

    add hasletter argument

--- assets/week-10/pca-code.R
+++ assets/week-10/pca-code.R
@@ -2,8 +2,10 @@
 
 d <- read.csv("http://andrewpbray.github.io/math-243/assets/data/handwritten.csv")
 
-plot_letter <- function(x) { # x should be a row from the handwritten data set
-  a <- as.numeric(x[, -1])
+plot_letter <- function(x, hasletter = TRUE) {
+  if(hasletter) {
+    a <- as.numeric(x[, -1])
+  }else{a <- as.numeric(x)}
   m <- matrix(a, nrow = 8, byrow = TRUE)
   m <- t(apply(m, 2, rev)) # rotate matrix
   image(m, axes = FALSE, col = rev(grey(seq(0, 1, length = 256))))
